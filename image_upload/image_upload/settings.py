c92163fb6e03614d3354d44bd95831ffb96822af
Talandar <talandar66@protonmail.com>
2023-09-21 16:33:41 +0200

    feat: added docker-compose with pgsql database

--- image_upload/image_upload/settings.py
+++ image_upload/image_upload/settings.py
@@ -73,13 +73,22 @@ WSGI_APPLICATION = 'image_upload.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
 
 # Password validation
